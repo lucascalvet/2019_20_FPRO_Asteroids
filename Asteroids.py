52cc1d04463b527c3e28c275e3dd85d8a5676f6f
Lucas Jorge Calvet de MagalhAPSes Fernandes Santos <up201904517@FEUP-B206D07.fe.up.pt>
2019-12-03 13:22:29 +0000

    Added firing ship effect and moving asteroid

--- Asteroids.py
+++ Asteroids.py
@@ -1,30 +1,39 @@
 import pygame
 import math
+import random
 
 pygame.init()
 WIDTH = 800
 HEIGHT = 600
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 asteroid = pygame.image.load('asteroid.png')
-blueship = pygame.image.load('ship.png')
+n_ship = pygame.transform.scale(pygame.image.load('ship.png'), (30, 20))
+f_ship = pygame.transform.scale(pygame.image.load('ship_firing.png'), (30, 20))
 big_ast = pygame.transform.scale(asteroid, (105, 100))
 med_ast = pygame.transform.scale(asteroid, (53, 50))
 small_ast = pygame.transform.scale(asteroid, (26, 25))
-ship = pygame.transform.scale(blueship, (30, 20))
+ship = n_ship
 bullet = pygame.transform.scale(pygame.image.load('bullet.png'), (5, 5))
 
-ship_x = 400
-ship_y = 300
-angle = 90
-bullet_angle = 0
-bullet_x = ship_x - bullet.get_width()/2 + 13 * math.cos(math.pi*angle/180)
-bullet_y = ship_y - bullet.get_height()/2 -  13 * math.sin(math.pi*angle/180)
-vel_x = 0
-vel_y = 0
-bullet_vel = 0
 VEL_ANG = 0.5
 ACCEL = 0.01
+MAX_VEL = 0.5
 FRICTION = 0.05
+AST_VEL = 0.1
+ship_x = 400
+ship_y = 300
+vel_x = 0
+vel_y = 0
+angle = 90
+bullet_x = ship_x - bullet.get_width()/2 + 13 * math.cos(math.pi*angle/180)
+bullet_y = ship_y - bullet.get_height()/2 - 13 * math.sin(math.pi*angle/180)
+bullet_vel = 0
+bullet_angle = 0
+ast1 = big_ast
+ast1_x = random.randrange(WIDTH + 1)
+ast1_y = random.randrange(HEIGHT + 1)
+ast1_ang = random.randrange(361)
+
 
 running = True
 clock = pygame.time.Clock()
@@ -38,6 +47,8 @@ while running:
     if keys[pygame.K_UP]:
         vel_x += ACCEL * math.cos(math.pi*angle/180) * dt
         vel_y += ACCEL * math.sin(math.pi*angle/180) * dt
+        ship = f_ship
+    else: ship = n_ship
 #    if keys[pygame.K_DOWN]:
     if vel_x > 0:
         vel_x = max(0, vel_x - FRICTION)
@@ -47,15 +58,23 @@ while running:
         vel_y = max(0, vel_y - FRICTION)
     if vel_y < 0:
         vel_y = min(0, vel_y + FRICTION)
-    vel_x = min(vel_x, 0.5*dt)
-    vel_x = max(vel_x, -0.5*dt)
-    vel_y = min(vel_y, 0.5*dt)
-    vel_y = max(vel_y, -0.5*dt)
+    vel_x = min(vel_x, MAX_VEL*dt)
+    vel_x = max(vel_x, -MAX_VEL*dt)
+    vel_y = min(vel_y, MAX_VEL*dt)
+    vel_y = max(vel_y, -MAX_VEL*dt)
 
     ship_x = (ship_x + vel_x) % WIDTH
     ship_y = (ship_y - vel_y) % HEIGHT
+    
     bullet_x = (bullet_x + bullet_vel * math.cos(math.pi*bullet_angle/180)) % WIDTH
     bullet_y = (bullet_y - bullet_vel * math.sin(math.pi*bullet_angle/180)) % HEIGHT  
+
+
+    ast1_x = (ast1_x + AST_VEL * math.cos(math.pi*ast1_ang/180) * dt) % WIDTH
+    ast1_y = (ast1_y + AST_VEL * math.sin(math.pi*ast1_ang/180) * dt) % HEIGHT
+
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -70,13 +89,15 @@ while running:
     if running == False:
         break
 
-
     rot_ship = pygame.transform.rotate(ship, angle)
     dw = rot_ship.get_width() - ship.get_width()
     dh = rot_ship.get_height() - ship.get_height()
     display.fill((0, 0, 0))
     display.blit(bullet, (bullet_x, bullet_y))
     display.blit(rot_ship, (ship_x-dw/2 - ship.get_width()/2, ship_y-dh/2 - ship.get_height()/2))
+    
+    display.blit(ast1, (ast1_x - ast1.get_width()/2, ast1_y - ast1.get_height()/2))
+    
     display.blit(big_ast, (0, 0))
     display.blit(med_ast, (105, 0))
     display.blit(small_ast, (158, 0))
