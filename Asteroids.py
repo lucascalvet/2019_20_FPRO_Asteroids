3c6967cb7f74a1ff006a2028bcfd300ee1780cdc
Lucas Calvet Santos <lucascalvet2000@hotmail.com>
2020-01-07 13:28:52 +0000

    Added comments. Minor adjustments

--- Asteroids.py
+++ Asteroids.py
@@ -4,50 +4,59 @@ import random
 PI = math.pi
 
 
-def rand_x():
+def rand_x():  # returns a random x coordinate within the game display
     return random.randrange(WIDTH + 1)
 
 
-def rand_y():
+def rand_y():  # returns a random y coordinate within the game display
     return random.randrange(HEIGHT + 1)
 
 
-def rand_ang(amp=(0, 2*PI)):
+def rand_ang(amp=(0, 2*PI)):  # returns a random angle (amplitude can be set)
     return random.randrange(int(1000*amp[0]), int(1000*amp[1]))/1000
 
 
-def set_level(level):
+def set_level(level):  # generates a new level and returns the asteroids list
     asteroids = []
     for _ in range(level + 3):
         x, y = rand_x(), rand_y()
+        rect = pygame.Rect(0, 0, 0, 0)
+        rect.size = (80, 80)
+        rect.bottomright = (x, y)
         asteroids.append({'x': x, 'y': y, 'ang': rand_ang(), 'type': big_ast,
-                          'rect': big_ast.get_rect(bottomright=(x, y))})
+                          'rect': rect})
     return asteroids
 
 
-def break_ast(asteroids, ast, ast_index, score):
-    if ast['type'] != small_ast:
-        if ast['type'] == big_ast:
-            new_type = med_ast
-            score += 20
+def break_ast(asteroids, ast_del, score):
+    # 'break' the asteroids from ast_del and return the new
+    # asteroids list and the new score
+    new_ast = []
+    for ast in ast_del:
+        new_ast.append(asteroids[ast])
+        del asteroids[ast]
+    for ast in new_ast:
+        if ast['type'] != small_ast:
+            if ast['type'] == big_ast:
+                new_type = med_ast
+                score += 20
+            else:
+                new_type = small_ast
+                score += 50
+            asteroids.append({'x': ast['x'], 'y': ast['y'],
+                              'ang': rand_ang(amp=(ast['ang']-0.75, ast['ang']+0.75)),
+                              'type': new_type,
+                              'rect': new_type.get_rect(bottomright=(ast['x'], ast['y']))})
+            asteroids.append({'x': ast['x'], 'y': ast['y'],
+                              'ang': rand_ang(amp=(ast['ang']-0.75, ast['ang']+0.75)),
+                              'type': new_type,
+                              'rect': new_type.get_rect(bottomright=(ast['x'], ast['y']))})
         else:
-            new_type = small_ast
-            score += 50
-        asteroids.append({'x': ast['x'], 'y': ast['y'],
-                          'ang': rand_ang(amp=(ast['ang']-0.75, ast['ang']+0.75)),
-                          'type': new_type,
-                          'rect': new_type.get_rect(bottomright=(ast['x'], ast['y']))})
-        asteroids.append({'x': ast['x'], 'y': ast['y'],
-                          'ang': rand_ang(amp=(ast['ang']-0.75, ast['ang']+0.75)),
-                          'type': new_type,
-                          'rect': new_type.get_rect(bottomright=(ast['x'], ast['y']))})
-    else:
-        score += 100
-    del asteroids[ast_index]
+            score += 100
     return asteroids, score
 
 
-def start_screen():
+def start_screen():  # displays a start screen and returns the 'running' value
     start_message = pygame.font.SysFont(font_list, 100, bold=True, italic=False).render('ASTEROIDS', True, (255, 255, 255))
     info_message = font.render('Press s to start.', True, (255, 255, 255))  
     display.blit(start_message, start_message.get_rect(center=(WIDTH/2, HEIGHT/2 - 40)))
@@ -63,7 +72,7 @@ def start_screen():
                     return True
 
 
-def pause():
+def pause():  # displays a pause screen and returns the 'running' value
     pause_message = large_font.render('PAUSE', True, (255, 255, 255))
     info_message = font.render('Press q to quit or p to continue.', True, (255, 255, 255))
     display.blit(pause_message, pause_message.get_rect(center=(WIDTH/2, HEIGHT/2 - 40)))
@@ -80,7 +89,7 @@ def pause():
                     return True
 
 
-def game_over():
+def game_over():  # displays a game over screen and returns the 'running' value
     over_message = large_font.render('GAME OVER', True, (255, 255, 255))
     info_message = font.render('Press q to quit or r to restart.', True, (255, 255, 255))
     display.blit(over_message, over_message.get_rect(center=(WIDTH/2, HEIGHT/2 - 40)))
@@ -102,7 +111,7 @@ WIDTH = 800
 HEIGHT = 600
 SHIP_WIDTH = 30
 SHIP_HEIGHT = 20
-display = pygame.display.set_mode((WIDTH, HEIGHT))
+# load assets
 instructions = pygame.image.load('instructions.png')
 asteroid = pygame.image.load('asteroid.png')
 n_ship = pygame.transform.scale(pygame.image.load('ship.png'), (SHIP_WIDTH, SHIP_HEIGHT))
@@ -113,17 +122,22 @@ small_ast = pygame.transform.scale(asteroid, (26, 25))
 ship = n_ship
 rot_ship = ship
 bullet = pygame.transform.scale(pygame.image.load('bullet.png'), (6, 6))
-
+font_list = 'Corbel, Courier 10 Pitc, FreeMono, Garuda'
+font = pygame.font.SysFont(font_list, 30, bold=True, italic=False)
+large_font = pygame.font.SysFont(font_list, 60, bold=True, italic=False)
+# set USEREVENT codes
 HYPERSPACE = 24
 NEW_LEVEL = 25
 NEW_TRY = 26
 PROTECTION = 27
+# set constants
 ANG_VEL = 0.3
 ACCEL = 0.008
 MAX_VEL = 0.4
 FRICTION = 0.05
-AST_VEL = 0.01
+AST_VEL = 0.1
 BULLET_VEL = 0.3
+# set inicial values
 ship_x, ship_y = WIDTH/2, HEIGHT/2
 vel_x, vel_y = 0, 0
 angle = 90
@@ -138,19 +152,19 @@ changing = True
 ship_vis = True
 protected = True
 stars = [(rand_x(), rand_y()) for _ in range(100)]
+#
 clock = pygame.time.Clock()
+display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_icon(small_ast)
-font_list = 'Corbel, Courier 10 Pitc, FreeMono, Garuda'
-font = pygame.font.SysFont(font_list, 30, bold=True, italic=False)
-large_font = pygame.font.SysFont(font_list, 60, bold=True, italic=False)
-running = start_screen()
+pygame.display.set_caption('Asteroids')
+running = start_screen()  # show the start screen
 pygame.time.set_timer(PROTECTION, 300)
 
 while running:
     dt = clock.tick(30)
-    pygame.display.set_caption('FPS: ' + str(clock.get_fps()))
+#    pygame.display.set_caption('FPS: ' + str(clock.get_fps()))
     keys = pygame.key.get_pressed()
-
+    # monitor the left, right and up keys
     if keys[pygame.K_LEFT]:
         angle += ANG_VEL * dt
     if keys[pygame.K_RIGHT]:
@@ -161,7 +175,7 @@ while running:
         ship = f_ship
     else:
         ship = n_ship
-
+    # apply friction to the ship
     if vel_x > 0:
         vel_x = max(0, vel_x - FRICTION)
     if vel_x < 0:
@@ -170,29 +184,30 @@ while running:
         vel_y = max(0, vel_y - FRICTION)
     if vel_y < 0:
         vel_y = min(0, vel_y + FRICTION)
+    # set a maximum velocity to the ship
     vel_x = min(vel_x, MAX_VEL*dt)
     vel_x = max(vel_x, -MAX_VEL*dt)
     vel_y = min(vel_y, MAX_VEL*dt)
     vel_y = max(vel_y, -MAX_VEL*dt)
+    # update ship position
     ship_x = (ship_x + vel_x) % (WIDTH + SHIP_WIDTH)
     ship_y = (ship_y - vel_y) % (HEIGHT + SHIP_HEIGHT)
+    # update bullets position and delete the ones that have reached the maximum distance
     bull_index = -1
-    ast_index = -1
-    color = (0, 255, 0)
-    ship_rect = rot_ship.get_rect(center=(ship_x-SHIP_WIDTH/2, ship_y-SHIP_HEIGHT/2))
-
+    bull_del = []  # list of bullets indexes to be deleted
     for bull in bullets:
         bull_index += 1
         if bull['d'] > bullet_max:
-            del bullets[bull_index]
-            break
+            bull_del.append(bull_index)
         bull['x'] = (bull['x'] + BULLET_VEL*dt * math.cos(PI*bull['ang']/180)) % WIDTH
         bull['y'] = (bull['y'] - BULLET_VEL*dt * math.sin(PI*bull['ang']/180)) % HEIGHT
         bull['d'] += BULLET_VEL*dt
         bull['rect'] = bullet.get_rect(topleft=(bull['x'], bull['y']))
-
+    for bull in bull_del:
+        del bullets[bull]
+    # handle events
     for event in pygame.event.get():
-        print(event)
+#        print(event)
         if event.type == pygame.QUIT:
             running = False
         if event.type == PROTECTION:
@@ -223,47 +238,51 @@ while running:
             angle = 90
             pygame.time.set_timer(NEW_TRY, 0)
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE:  # shoot bullet
                 if len(bullets) < 4 and ship_vis:
                     x = ship_x - SHIP_WIDTH/2 - bullet.get_width()/2 + SHIP_HEIGHT*math.cos(PI*angle/180)
                     y = ship_y - SHIP_HEIGHT/2 - bullet.get_height()/2 - SHIP_HEIGHT*math.sin(PI*angle/180)
                     bullets.append({'x': x, 'y': y, 'ang': angle, 'd': 0,
                                     'rect': bullet.get_rect(topleft=(x, y))})
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN:  # spawn the ship in a random position
                 if ship_vis:
                     ship_vis = False
                     pygame.time.set_timer(HYPERSPACE, 1000)
-            if event.key == pygame.K_q:
+            if event.key == pygame.K_q:  # quit the game
                 running = False
-            if event.key == pygame.K_p:
+            if event.key == pygame.K_p:  # pause the game
                 running = pause()
-
+    # handle collisions with asteroids (from both bullets and the ship)
+    color = (0, 255, 0)
+    ship_rect = rot_ship.get_rect(center=(ship_x-SHIP_WIDTH/2, ship_y-SHIP_HEIGHT/2))
+    ast_index = -1
+    ast_del = []  # list of asteroids indexes to be deleted
     for ast in asteroids:
         ast_index += 1
         ast['x'] = (ast['x'] + AST_VEL*dt * math.cos(ast['ang'])) % (WIDTH + ast['type'].get_width())
         ast['y'] = (ast['y'] - AST_VEL*dt * math.sin(ast['ang'])) % (HEIGHT + ast['type'].get_height())
-        ast['rect'] = ast['type'].get_rect(bottomright=(ast['x'], ast['y']))
+        ast['rect'].bottomright=(ast['x'], ast['y'])
+        print(ast['rect'].center)
         bull_coll = ast['rect'].collidelist([bull['rect'] for bull in bullets])
         ship_coll = ast['rect'].colliderect(ship_rect)
         if bull_coll > -1:
-            asteroids, score = break_ast(asteroids, ast, ast_index, score)
+            ast_del.append(ast_index)
             del bullets[bull_coll]
-            break
         if ship_coll and not changing and ship_vis:
             color = (255, 0, 0)
             lives -= 1
-            asteroids, score = break_ast(asteroids, ast, ast_index, score)
+            ast_del.append(ast_index)
             if lives > 0:
                 changing = True
                 ship_vis = False
                 pygame.time.set_timer(NEW_TRY, 2000)
-            break
-
+    asteroids, score = break_ast(asteroids, ast_del, score)
+    # start a new level when there are no asteroids
     if len(asteroids) == 0 and not changing and lives > 0:
         changing = True
         level += 1
         pygame.time.set_timer(NEW_LEVEL, 2000)
-
+    # display every thing on the screen
     rot_ship = pygame.transform.rotate(ship, angle)
     dw = rot_ship.get_width() - SHIP_WIDTH
     dh = rot_ship.get_height() - SHIP_HEIGHT
@@ -276,7 +295,7 @@ while running:
     for ast in asteroids:
         display.blit(ast['type'], (ast['x'] - ast['type'].get_width(), ast['y'] - ast['type'].get_height()))
         pygame.draw.rect(display, (0, 255, 0), ast['rect'], 3)
-        pygame.draw.circle(display, (255, 0, 0), (int(ship_x-SHIP_WIDTH/2), int(ship_y-SHIP_HEIGHT/2)), 2)
+#        pygame.draw.circle(display, (255, 0, 0), ast['rect'].center, 2)
     if ship_vis:
         display.blit(rot_ship, (ship_x-dw/2 - SHIP_WIDTH, ship_y-dh/2 - SHIP_HEIGHT))
 #    pygame.draw.circle(display, (0, 255, 0), (int(ship_x-SHIP_WIDTH/2), int(ship_y-SHIP_HEIGHT/2)), 2)
@@ -285,6 +304,7 @@ while running:
     display.blit(font.render('Score: ' + str(score), True, (255, 255, 255)), (10, 40))
     display.blit(font.render('Lives: ' + str(lives), True, (255, 255, 255)), (10, 70))
     pygame.display.flip()
+    # if there are no lives left, display the game over screen
     if lives == 0:
         running = game_over()
         score = 0
